80ae06e1418fc01e314b11647682aeedde88663d
Edward Simmy <simmyedward4@gmail.com>
2025-03-10 04:45:41 +0300

    finished ex 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,56 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+    
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        int numGuesses = 0;
+
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("No this is too low, Guess Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Almost there, Try to Guess Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it! you took " + numGuesses + "guesses" );
+            }
+            string Response;
+            while (numGuesses > 6 )
+            
+            {
+                Console.Write("You Guessed more then 6 times wrongly. GAME OVER!");
+            }   
+                
+            do  
+            {
+                Console.Write("Do you wish to start again?  ");
+                Response = Console.ReadLine();
+
+            }
+
+            while (Response == "yes");
+
+            {
+                Console.Write("What is your guess?"  );
+            }  
+
+        }                    
     }
 }
