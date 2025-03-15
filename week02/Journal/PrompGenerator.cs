a5a3eb7589943d64380695df4d6b13d0ffc70cc5
Edward Simmy <simmyedward4@gmail.com>
2025-03-15 23:12:38 +0300

    finished

---
+++ week02/Journal/PrompGenerator.cs
@@ -0,0 +1,24 @@
+
+
+using System;
+
+public class PromptGenerator {
+public List<string> Prompts {get; set;}
+
+    public PromptGenerator() {
+        Prompts = new List<string> {
+            "What was the best part of your day?",
+            "Who was the most interesting person you interacted with today?",
+            "Write about something that happened for which you are grateful today:",
+            "If you had one thing you could do over today, what would it be?"
+        };
+    }
+
+    //Generates a random prompt from the list
+    public string GenerateRandomPrompt() {
+        Random random = new Random();
+        int index = random.Next(Prompts.Count);
+        return Prompts[index];
+    }
+
+}
---
+++ week02/Resumes/job.cs
@@ -0,0 +1,12 @@
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        public._string_givenName="Edward";
+        public._string_familyName="Simmy";
+        job._jobTitle="software Engineer";
+    }
+}
